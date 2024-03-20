6d08c14be67707ffd6c68ad9c084cfdd9e1a114a
Ruffman <44611474+Ruffman@users.noreply.github.com>
2024-03-20 16:51:30 +0100

    the tank class now rotates towards the mouse position it now has mount points to attach weapon systems (basically only coordinates for now)

--- tank.py
+++ tank.py
@@ -1,7 +1,9 @@
 from typing import Any
 import pygame as pg
+import math
 
 from projectile import Projectile
+from weapon import WeaponSystem, Turret
 from game_internal import TYPE, ShipType, SHIP_TYPE_STATS, Faction
 
 
@@ -16,27 +18,67 @@ class Tank(pg.sprite.Sprite):
         self.size = SHIP_TYPE_STATS[self.ship_type][0]
         self.speed = SHIP_TYPE_STATS[self.ship_type][1]
 
-        self.image = pg.transform.scale(pg.image.load(TYPE[faction]['image']).convert(), (self.size, self.size))
+        self.orig_image = pg.transform.scale(pg.image.load(TYPE[faction]['image']).convert(), (self.size, self.size))
+        self.image = self.orig_image
         self.rect = self.image.get_rect()
         self.radius = self.size // 2
 
-        self.projectile: Projectile
+        self.mount_points: [pg.Vector2] = [pg.Vector2(-self.size // 3, -self.size // 3),
+                                           pg.Vector2(self.size // 3, -self.size // 3)]
+        self.weapons: [WeaponSystem] = []
+        for pt in self.mount_points:
+            self.weapons.append(Turret(pt))
 
+        self.rotation_angle: float = 0
+        self.orig_heading: pg.Vector2 = pg.Vector2(0, -1)
+        self.heading: pg.Vector2 = pg.Vector2(0, 0)
 
     def update(self, *args: Any, **kwargs: Any) -> None:
-        pass
+        self.rotate_towards_mouse()
+
+        self.draw_weapons()
+        self.draw_heading()
 
     def move(self, direction: pg.Vector2, dt):
         self.rect.center += direction * dt * self.speed
 
-    def fire(self, target: pg.Vector2) -> Projectile:
-        spawn_point = pg.Vector2(self.rect.center)
-        direction = pg.Vector2(target - spawn_point).normalize()
-        projectile = Projectile(spawn_point, direction, self.faction)
-        return projectile
+    def fire(self, target: pg.Vector2) -> [Projectile]:
+        new_projectiles = []
+        for weapon in self.weapons:
+            spawn_point = pg.Vector2(self.rect.center + weapon.current_position)
+            direction = pg.Vector2(target - spawn_point).normalize()
+            projectile = Projectile(spawn_point, direction, self.faction)
+            if projectile:
+                new_projectiles.append(projectile)
+        return new_projectiles
 
     def get_position(self) -> pg.Vector2:
         return pg.Vector2(self.rect.center)
+
+    def rotate_towards_mouse(self):
+        direction: pg.Vector2 = pg.Vector2(pg.mouse.get_pos()) - pg.Vector2(self.rect.center)
+        self.rotation_angle = self.orig_heading.angle_to(direction)
+
+        old_center = self.rect.center
+        self.image = pg.transform.rotate(self.orig_image, -self.rotation_angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = old_center
+        self.heading = direction
+
+        for weapon in self.weapons:
+            weapon.rotate(self.rotation_angle)
+
+    def draw_heading(self):
+        length = 200
+        start = self.rect.center
+        end = start + self.heading.normalize() * length
+        pg.draw.line(self.surface, "green", start, end)
+
+    def draw_weapons(self):
+        for weapon in self.weapons:
+            weapon_pos = self.image.get_rect().center + weapon.current_position
+            pg.draw.circle(self.image, "white", weapon_pos, 5)
+            pg.draw.line(self.surface, "red", self.rect.center + weapon.current_position, pg.mouse.get_pos())
 
 
 class Crosshair(pg.sprite.Sprite):
@@ -69,7 +111,7 @@ class AiTank(Tank):
         self.fire_cd -= dt
         self.circle_target(dt, pg.Vector2(target.rect.center))
 
-        pg.draw.line(pg.display.get_surface(), "red", self.rect.center, target.rect.center)
+        pg.draw.line(self.surface, "red", self.rect.center, target.rect.center)
 
     def fire(self, target: pg.Vector2) -> Projectile:
         if self.fire_cd <= 0:
