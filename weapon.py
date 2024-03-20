3b8e0e3939b91db288a6850c1986abf4a056011a
Ruffman <44611474+Ruffman@users.noreply.github.com>
2024-03-20 16:48:04 +0100

    a way to rotate weapons

--- weapon.py
+++ weapon.py
@@ -7,7 +7,9 @@ class WeaponSystem(pg.sprite.Sprite):
     def __init__(self, position: pg.Vector2):
         super().__init__()
 
-        self.mount_position = position
+        self.mount_position: pg.Vector2 = position
+        self.current_position: pg.Vector2 = self.mount_position
+        self.rotation_angle: float = 0
 
     def update(self, *args: Any, **kwargs: Any) -> None:
         pass
@@ -19,6 +21,10 @@ class Turret(WeaponSystem):
 
     def update(self, *args: Any, **kwargs: Any) -> None:
         pass
+
+    def rotate(self, angle: float):
+        self.rotation_angle = angle
+        self.current_position = self.mount_position.rotate(angle)
 
 
 class Bay(WeaponSystem):
